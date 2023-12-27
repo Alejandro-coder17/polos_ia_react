47b472fa7ff546dd04d8e0026412768378ac07f4
Alejandro-coder17 <andre_espinoza_ortiz@hotmail.com>
2023-12-27 17:34:28 -0500

    Colocando color, archivos y personalizando logo. Además, se agrego back con index inicial.

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import state from '../store'
 import { useSnapshot } from 'valtio'
+import { getContrastingColor } from '../config/helpers'
 
 const CustomButton = ({ type, title, handleClick, customStyles }) => {
   
@@ -11,7 +12,13 @@ const CustomButton = ({ type, title, handleClick, customStyles }) => {
         if(type === 'filled') {
             return {
                 background: snap.color,
-                color: '#000',
+                color: getContrastingColor(snap.color)
+            }
+        } else if(type === 'outline'){
+            return {
+                borderWidth: '1px',
+                borderColor: snap.color,
+                color: snap.color
             }
         }
     }
